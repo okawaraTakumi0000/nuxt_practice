7d6200bdd221b2ae48b746a4d5609c881fa76c93
大河原拓実 <okawaratakumi@ookawaratakuminoMacBook-Pro.local>
2024-01-02 00:31:20 +0900

    first commit API通信まで

---
+++ pages/articles/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>記事だよー</div>
+</template>
+
+<script setup>
+
+const {data} = await useFetch('https://qiita.com/api/v2/items',{method:'GET',headers:{authorization: }})
+console.log(data.value)
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <NuxtLink to="/articles">記事一覧</NuxtLink>
+</template>
